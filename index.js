0a6d73474c943f39b844ff0d44113bae43a1f62d
n304Sc-haoran <3915264929@qq.com>
2025-09-13 00:26:11 +0800

    更新sh启动文件的退出模式

--- index.js
+++ index.js
@@ -4,21 +4,19 @@ const fs = require('fs');
 const {
   _log,
   setFile,
-  input
+  input,
+  getP,
+  Exit
 } = require('./lib/tool.js');
-const net = require('net'); // 引入net模块用于检测端口
-let PORT = server[2];
 setFile("./lib/data/exit.log", "1")
-function Exit() {
-  _log('清理缓存中')
-  fs.writeFileSync('./lib/data/verify.json', '{}')
-  fs.writeFileSync('./lib/data/main.log', '')
-  fs.writeFileSync("./lib/data/exit.log", "Close")
-  server[0].close(() => {
-    _log('Server has been closed');
-  });
-  throw new Error('Close');
-}
+/*
+  exit.log 内容：
+ * 1 代表正在运行
+ * 2 表示因错误退出
+ * 3 表示正常退出
+ * 4 表示正在重启
+*/
+const config = fs.readFileSync('./start-setting.txt').toString()
 async function isPortAvailable(port) {
   return new Promise((resolve) => {
     const tester = net.createServer()
@@ -39,17 +37,16 @@ async function getAvailablePort(startPort) {
   }
   throw new Error('POSTS is all using');
 }
+const net = require('net'); // 引入net模块用于检测端口
 async function main() {
-  try {
-    PORT = await getAvailablePort(PORT);
-    server[0].listen(PORT, '0.0.0.0', () => {
-      _log(`Web server is running on ${server[1]} (Port: ${PORT})`);
+  const value = getP(config, 'PORT')
+  typeof value !== 'number' ? (() => {
+    Exit('配置文件错误：PORT 的值非数字')
+  })() : getAvailablePort(Math.floor(value)).then(e => {
+    server[0].listen(e, '0.0.0.0', () => {
+      _log(`Web server is running on http://${server[1]}:${e} (Port: ${e})`);
     });
-  } catch (err) {
-    _log(`Failed to start server: ${err.message}`, 'error');
-    Exit()
-  }
+  })
 }
-process.on('SIGINT', Exit);
-
 main()
+process.on('SIGINT', Exit);
--- start.sh
+++ start.sh
@@ -1,15 +1,22 @@
 echo -e "\n按 CTRL+C 退出"
 while true; do
   node index.js
-  exit_code=$?
-  if [ -f "lib/data/exit.log" ] && 
-     [ "$(tr -d '\n\r' < lib/data/exit.log)" = "Close" ]; then
-    echo "成功退出"
-    rm -f "lib/data/exit.log"
-    break
-  fi
-  if [ $exit_code -ne 0 ]; then
-    echo -e "\n⚠️ 进程异常退出 (代码: $exit_code)，3秒后重启..."
-    sleep 3
+  if [ -f "lib/data/exit.log" ];then
+    file="$(tr -d '\n\r' < lib/data/exit.log)";
+    case $file in
+      "1")
+        echo ""
+        ;;
+      "2")
+        echo "因错误退出！"
+        ;;
+      "3")
+        echo "已退出"
+        break
+        ;;
+      "4")
+        echo "已重启"
+        ;;
+esac
   fi
 done
