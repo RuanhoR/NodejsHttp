afcd759aa9b229a66c6cecaa21af85bec93994a6
n304Sc-haoran <3915264929@qq.com>
2025-09-21 19:02:54 +0800

    更新0.2.4

--- html/lib/mcbe-cb/main.js
+++ html/lib/mcbe-cb/main.js
@@ -1,13 +1,1 @@
-import {
-  config
-} from "/filesD/lib/mcbe-cb/config.js";
-const ui = document.getElementById("app")
-const div = document.createElement("div")
-const code = document.createElement("input")
-code.type = "text";
-code.style.position = "absolute";
-code.style.width = "100%"
-code.style.top = "90%"
-ui.appendChild(div)
-ui.appendChild(code)
-document.write("e")
+import{config}from"/filesD/lib/mcbe-cb/config.js";const ui=document.getElementById("app"),div=document.createElement("div"),code=document.createElement("input");code.type="text",code.style.position="absolute",code.style.width="100%",code.style.top="90%",ui.appendChild(div),ui.appendChild(code),document.write("e");
