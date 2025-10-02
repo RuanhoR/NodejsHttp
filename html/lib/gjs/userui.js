b7e4aca7f57349ad27d88e8e2fa0dc0cdb4ae37b
n304Sc-haoran <3915264929@qq.com>
2025-10-03 00:48:47 +0800

    啊恰

--- html/lib/gjs/userui.js
+++ html/lib/gjs/userui.js
@@ -1,25 +1,81 @@
 import {
   createPage
 } from "/filesD/lib/menu-v2.js"
+const AllSetting = [
+  "name",
+  "mail"
+]
+const Apiurl = "/login";
+const dialogName = {
+  class: "dialog",
+  content: [
+    {
+      type: "p",
+      title: "test",
+      id: "asd"
+    }
+  ]
+};
 export class userUi {
   constructor(data) {
+    this.data = data
     this.page = new createPage({
       switch: [
         {
           title: "基础设置",
           id: "home",
-          canSwitch: true
+          canSwitch: true,
+          button: [
+            {
+              id: "set-user-name",
+              run: (Event,page)=>this.ClickSet(0,page)
+            }
+          ]
         }
-      ],
-      page: "<h1 class='t1'>从左侧栏目获取或修改信息</h1>"
+      ]
     })
     this.page.loadCSS("/filesD/lib/gcss/user.css")
-    this.page.PageContentSeter("home",``)
-    this.page.setPage("home")
+    this.page.PageContentSeter("home",`<div class="group">
+    <div class="tip">用户名</div>
+    <div id="t" class="group-fixed">
+      <div id="username">${data.name}</div>
+      <div id="set-user-name">
+        更改
+      </div>
+    </div>
+  </div>`)
+    this.page.setChildById("home")
   }
-  htmlContent(name) {
-    return {
-      "home": ``
-    }[name] || "";
+  JSONPOSTreq(url, content, callback) {
+    try {
+      fetch(url, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          credentials: 'include',
+          body: JSON.stringify(content)
+        })
+        .then((res) => res.json())
+        .then(callback);
+    } catch (err) {
+      callback({
+        code: -1,
+        msg: "网络错误"
+      });
+    }
+  }
+  async ClickSet(name,page) {
+    const setting = AllSetting[name]
+    if (typeof name !== "number" || !setting) throw new TypeError("ERR_INPUT")
+    switch (name) {
+      case 0:
+        const res = await page.dialog(dialogName).RDAOpen()
+        break;
+      case 1:
+        break;
+      case 2:
+        break;
+    }
   }
 }
